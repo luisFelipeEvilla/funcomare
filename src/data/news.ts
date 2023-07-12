70a54e10839be9c1c8a292e1971786536ea39ee3
Luis Felipe Evilla <luisevilla588@gmail.com>
2023-07-12 02:59:27 -0500

    update news info

--- src/data/news.ts
+++ src/data/news.ts
@@ -22,8 +22,15 @@ const news = [
     {
         id: 2,
         title: 'Talleres de formación en el centro educativo distrital la hermosa',
-        description: `
-        lorem ipsum dolor sit amet, consectetur adipiscing elit.`,
+        description: `Con el propósito de responder a un sin número de necesidades dentro del contexto social, 
+        cívico y educativo, para el futuro de las nuevas generaciones, el licenciado Uvaldo Mercado Calabria, 
+        docente de la Universidad del Magdalena, dio a conocer la obra literaria Competencias, Convivencias
+        y Cultura Ciudadana "El Buen Samario".\n
+         
+        El libro busca  fomentar la lectura en los estudiantes desde la primera infancia, con el fin de contribuir 
+        en el ámbito educativo a continuar fomentando la gestión del conocimiento, para lograr  un mejor nivel en los 
+        resultados de las Pruebas de Estado Saber 11 y Saber Pro. 
+        `,
         frontPage: '/images/mission-banner.jpg',
         images: [
             '/images/mission-banner.jpg',
@@ -48,8 +55,15 @@ const news = [
     {
         id: 2,
         title: 'Talleres de formación en el centro educativo distrital la hermosa',
-        description: `
-        lorem ipsum dolor sit amet, consectetur adipiscing elit.`,
+        description: `Con el propósito de responder a un sin número de necesidades dentro del contexto social, 
+        cívico y educativo, para el futuro de las nuevas generaciones, el licenciado Uvaldo Mercado Calabria, 
+        docente de la Universidad del Magdalena, dio a conocer la obra literaria Competencias, Convivencias
+        y Cultura Ciudadana "El Buen Samario".\n
+         
+        El libro busca  fomentar la lectura en los estudiantes desde la primera infancia, con el fin de contribuir 
+        en el ámbito educativo a continuar fomentando la gestión del conocimiento, para lograr  un mejor nivel en los 
+        resultados de las Pruebas de Estado Saber 11 y Saber Pro. 
+        `,
         frontPage: '/images/mission-banner.jpg',
         images: [
             '/images/mission-banner.jpg',
